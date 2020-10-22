a6968a41ea941e898a3dbf38c82e5b45bb0f27e9
njwood <woodn123@gmail.com>
2020-10-22 14:09:41 -0400

    centers some items on the page, changes the bkg color

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import './App.css';
-import { Button, TextField, Select, MenuItem, FormControl, FormHelperText, Container, FormGroup, FormControlLabel } from '@material-ui/core';
+import { Button, TextField, Select, MenuItem, FormControl, FormHelperText, Container, FormGroup, FormControlLabel, InputLabel } from '@material-ui/core';
 
 function App() {
 
-  const [login, setLoginParameter] = React.useState('');
-
-  const handleChange = (event) => {
-    setLoginParameter(event.target.value);
-    console.log(event.target.value)
-  };
-
   return (
-    <div>
-      <label>Sign Into 7Factor Login</label>
-      
+    <div class="ParentClass">
+      <div class="Children">
+        <h1>7Factor Login Page</h1>
+        <div class="Child">
+          <FormControl variant="outlined">
+            <InputLabel htmlFor="outlined-age-native-simple">Login Method</InputLabel>
+            <Select native label="Login Method">
+              <option value={"AzureAD"}>AzureAD</option>
+              <option value={"7Factor"}>7Factor</option>
+            </Select>
+          </FormControl>
+        </div>
+        <div class="Child">
+          <TextField required id="username-required" label="Username" variant="outlined"></TextField>
+        </div>
+        <div class="Child">
+          <TextField required id="password-required" label="Password" variant="outlined"></TextField>
+        </div>
+        <div class="Child">
+          <Button variant="outlined"> Submit </Button>
+        </div>
+
+      </div>
     </div>
   );
 }
