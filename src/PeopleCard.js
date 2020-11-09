0980fe7e09318a874cbd44721c6fc66a7be73f2b
njwood <woodn123@gmail.com>
2020-11-09 14:54:21 -0500

    fixes css for most of the carD

--- src/PeopleCard.js
+++ src/PeopleCard.js
@@ -3,14 +3,27 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Avatar , Card, CardActions, CardContent} from '@material-ui/core';
 import './PeopleCard.css';
 
+// Data to display here is to be obtained through a JSON through the database per user
+// Every instance of the card will make its own db call for the data
+
+const useStyles = makeStyles({
+    root: {
+        maxWidth: 400,
+        maxHeight: 400,
+    }
+});
+
 function PeopleCard(props) {
+
+    const classes = useStyles();
+
     return (
         <div class="ParentDiv">
-                <Card varient="outlined">
+                <Card varient="outlined" className={classes.root}>
                     <CardContent>
                     <div class="TopRow">
                         <Avatar alt = "default image" src="./DefaultAvatar.jpg"/>
-                        <label> Employee Name </label>
+                        <h2> Employee Name </h2>
                     </div>
                     <div class="MidRow">
                         <label>Average work time per week: </label>
